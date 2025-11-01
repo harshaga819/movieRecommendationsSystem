d97a73974273b2a0db61c60f710c322d466e9203
Harsh Agarwal <147265675+harshaga819@users.noreply.github.com>
2025-11-01 15:26:51 +0530

    Update download_files.py

--- utils/download_files.py
+++ utils/download_files.py
@@ -4,7 +4,7 @@ import gdown
 def download_large_files():
     files = {
         "model/tag_sim.npy": "https://drive.google.com/uc?export=download&id=YOUR_TAG_SIM_FILE_ID",
-        "model/movie_tag_matrix.pkl": "https://drive.google.com/uc?export=download&id=YOUR_MOVIE_TAG_MATRIX_FILE_ID",
+        "model/movie_tag_matrix.pkl": "https://drive.google.com/uc?export=download&id=1l99nPAKwbEhiCZLSncsK7i9d8x_3tfyT",
     }
 
     for file_path, url in files.items():
